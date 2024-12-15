34acf89fccdb6ed43bfe58503366445c54e288b8
SahirRadzi <sahir.radzi@gmail.com>
2024-12-15 16:51:41 +0800

    Setup Livewire Login -OnProgress/part32-10:07

--- app/Livewire/Auth/Login.php
+++ app/Livewire/Auth/Login.php
@@ -3,12 +3,44 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
 class Login extends Component
 {
+    public $email = '';
+    public $password = '';
+    public $showPassword = false;
+
+    protected $rules = [
+        'email' => 'required|email',
+        'password' => 'required|min:8',
+    ];
+
+    protected $messages = [
+        'email.required' => 'Email is required',
+        'email.email' => 'Format email not valid',
+        'password.required' => 'Password is required',
+        'password.min' => 'Password must be 8 characters',
+    ];
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function togglePassword()
+    {
+        $this->showPassword = !$this->showPassword;
+    }
+
+    public function login()
+    {
+        $this->validate();
+    }
+
+    #[layout('components.layouts.app', ['hideBottomNav' => true])]
     public function render()
     {
-        return view('livewire.auth.login')
-            ->layout('components.layouts.app', ['hideBottomNav' => true]);
+        return view('livewire.auth.login');
     }
 }
--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -3,12 +3,13 @@
 namespace App\Livewire\Auth;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
 class Register extends Component
 {
+    #[layout('components.layouts.app', ['hideBottomNav' => true])]
     public function render()
     {
-        return view('livewire.auth.register')
-            ->layout('components.layouts.app', ['hideBottomNav' => true]);
+        return view('livewire.auth.register');
     }
 }
